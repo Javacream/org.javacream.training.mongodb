cae8d9d6430c391b9b60650758f01367dd47f6cb
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-04-27 10:32:35 +0200

    add new features in publishing

--- publishing.js
+++ publishing.js
@@ -1,37 +1,53 @@
 //Publisher example
-function setUp(){
+function getDb(){
     let conn = new Mongo()
     return conn.getDB("publishing")
 }
 
-function tearDown(db){
+function setUpPublishing(){
+    let db = getDb()
+    let publisher1 = {name: "Springer", address: {city:"Berlin", street: "Alexanderplatz"}}
+    let publisher2 = {name: "Addison", address: {city:"New York", street: "Central Park"}}
+    db.publishers.insertOne(publisher1)
+    db.publishers.insertOne(publisher2)
+    const books = []
+
+    for (let i = 0; i < 10; i++){
+        books.push({isbn: "ISBN" + i, title: "Title"+i, price:9.99*i, pages: 100*i})
+    }
+
+    books[5].publishingDate = new Date()
+    books[3].inStock = true
+    db.books.insertMany(books)
+
+}
+
+function tearDownPublishing(){
+    let db = getDb()
     db.publishers.drop()
     db.books.drop()
 
 }
 function publishersExample(){
-    let db = setUp()
-    let publisher1 = {name: "Springer", address: {city:"Berlin", street: "Alexanderplatz"}}
-    let publisher2 = {name: "Addison", address: {city:"New York", street: "Central Park"}}
-    db.publishers.insertOne(publisher1)
-    db.publishers.insertOne(publisher2)
-    db.publishers.find({name:"Springer"}).forEach(printjson)
-    db.publishers.find({"address.city": "Berlin"})
+    let db = getDb()
+    printCursor(db.publishers.find({name:"Springer"}))
+    printCursor(db.publishers.find({"address.city": "Berlin"}))
     db.publishers.updateOne({name:"Springer"}, {$set: {phone: "030 12345"}})
     db.publishers.updateOne({name:"Springer"}, {$set: {"address.city": "München"}})
-    db.publishers.find({name:"Springer"}).forEach(printjson)
-    db.publishers.find({phone:null}).forEach(printjson)
-    db.publishers.find({phone: {$ne: null}}).forEach(printjson)
-    tearDown(db)
+    printCursor(db.publishers.find({name:"Springer"}))
+    printCursor(db.publishers.find({phone:null}))
+    printCursor(db.publishers.find({phone: {$ne: null}}))
 }
 
 
 function booksExample(){
-    let db = setUp()
-    for (let i = 0; i < 10; i++){
-        db.books.insertOne({isbn: "ISBN" + i, title: "Title"+i, price:9.99*i, pages: 100*i})
-    }
-    db.books.find({price: {$gt: 39.99}}).forEach(printjson)
-    db.books.find({title: {$regex: /T.*7/}}).forEach(printjson)
-
+    let db = getDb()
+    printCursor(db.books.find({price: {$gt: 39.99}}))
+    printCursor(db.books.find({title: {$regex: /T.*7/}}))
+    printCursor(db.books.find({title: {"$in": ["Title3", "Title5"]}}, {price:1}))
 }
+
+function printCursor(cursor){
+    cursor.forEach(printjson)
+}
+
