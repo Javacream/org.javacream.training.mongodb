a2e261339300586ebf8fcac86dba9d922f70197c
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-04-27 14:22:50 +0200

    implement array updates

--- publishing.js
+++ publishing.js
@@ -36,15 +36,20 @@ function setUpPublishing(){
     books[2].sections = ["Introduction", "The Java programming language", "JEE"]
     books[3].sections = ["Cooking is easy", "Advanced Tea", "Advanced coffee using Java"]
     books[3].sections = ["Holidays in the sun", "Java Island"]
+    books[2].comments = [{author: "hugo", comment: "great", stars:5}, {author: "emil", comment: "not so great", stars:2}]
+    books[4].comments = [{author: "hugo", comment: "nonsense", stars:1}, {author: "emil", comment: "not so great", stars:2}, {author: "fritz", comment: "ok", stars:3}]
 
     db.books.insertMany(books)
 
+    db.authors.insertOne({lastname:"Sawitzki", firstname:"Rainer", email: ["training@rainer-sawitzki.de"]})
+    db.authors.insertOne({lastname:"Meier", firstname:"Frida", email: ["frida@meier.com"]})
 }
 
 function tearDownPublishing(){
     let db = getDb()
     db.publishers.drop()
     db.books.drop()
+    db.authors.drop()
 
 }
 function publishersExample(){
@@ -72,6 +77,30 @@ function booksExample(){
 
 }
 
+function booksCommentsExample(){
+    let db = getDb()
+    let query = {comments:{author: "hugo", comment: "nonsense", stars:1}}
+    printCursor(db.books.find( query))
+    query = {comments:{"$elemMatch": {author: "hugo", stars: {"$gt": 3}}}}
+    printCursor(db.books.find( query))
+
+}
+function addCommentToBook(){
+    let db = getDb()
+    let query = {title:"Title4"}
+    let update = {"$push": {comments: {author:"gregor", comment:"brilliant", stars: 4}}}
+    printjson(db.books.findOneAndUpdate(query, update).comments)
+
+}
+
+function authorEmailExample(){
+    printCursor(db.authors.find())
+    let db = getDb();
+    db.authors.updateOne({lastname: "Sawitzki"}, {"$addToSet": {email: "training@rainer-sawitzki.de"}})
+    db.authors.updateOne({lastname: "Meier"}, {"$addToSet": {email: "frida@meier.de"}})
+    printCursor(db.authors.find())
+
+}
 function printCursor(cursor){
     cursor.forEach(printjson)
 }
