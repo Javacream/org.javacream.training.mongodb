5cac6767545a9aea71949f99939b67bdb46e82bd
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-11-16 13:59:52 +0100

    introduce $lte

--- src/playground-publishing_select_update.mongodb.js
+++ src/playground-publishing_select_update.mongodb.js
@@ -1,18 +1,17 @@
-use ('training')
+use('training')
 
-function printCursor(cursor){
-    cursor.forEach((e) => console.log(JSON.stringify(e)))
+function printCursor(cursor) {
+  cursor.forEach((e) => console.log(JSON.stringify(e)))
 }
-function printDocument(doc){
+function printDocument(doc) {
   console.log(JSON.stringify(doc))
 }
 
 
-//printCursor(db.data.find({title: "Title2", price: 2.99})) //ein Treffer
-//printCursor(db.data.find({title: "Title2", price: 3.99})) //kein Treffer
-//printCursor(db.data.find({name: 'Springer'}))
+let criterion = { price: 
+                        { 
+                          '$lte': 2.99
+                        } 
+                }
 
-//Ich möchte alle Dokumente haben, die sich auf einer Berliner Addresse beziehen
-//printCursor(db.data.find({city: 'Berlin'})) //kein Treffer, city müsste Top Level sein
-//printCursor(db.data.find({address.city: 'Berlin'})) //Syntax Error
-//printCursor(db.data.find({'address.city': 'Berlin'})) //Trffer
+printCursor(db.data.find(criterion)) //drei Treffer
