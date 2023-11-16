012c99edba214aa79c456147ef2c385423851d83
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-11-16 13:45:40 +0100

    play with criterions

--- src/playground-publishing_select_update.mongodb.js
+++ src/playground-publishing_select_update.mongodb.js
@@ -7,7 +7,12 @@ function printDocument(doc){
   console.log(JSON.stringify(doc))
 }
 
-//printCursor(db.publishers.find())
 
-let criterion = {title: "Title2"}
-printCursor(db.data.find(criterion))
+//printCursor(db.data.find({title: "Title2", price: 2.99})) //ein Treffer
+//printCursor(db.data.find({title: "Title2", price: 3.99})) //kein Treffer
+//printCursor(db.data.find({name: 'Springer'}))
+
+//Ich möchte alle Dokumente haben, die sich auf einer Berliner Addresse beziehen
+//printCursor(db.data.find({city: 'Berlin'})) //kein Treffer, city müsste Top Level sein
+//printCursor(db.data.find({address.city: 'Berlin'})) //Syntax Error
+//printCursor(db.data.find({'address.city': 'Berlin'})) //Trffer
