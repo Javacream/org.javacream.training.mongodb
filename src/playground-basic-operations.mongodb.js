0fdb2c9c6314c3b62216daaa98dc701698a38b68
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2023-11-16 12:10:30 +0100

    remove unnecessary (?) variables

--- src/playground-basic-operations.mongodb.js
+++ src/playground-basic-operations.mongodb.js
@@ -1,28 +1,25 @@
 use('training')
 db.data.drop()
 
-let sawitzki_document = {
-    'lastname': 'Sawitzki', 
-    'firstname': ['Rainer', 'Ulrich'], 
+
+db.data.insertMany([{
+    'lastname': 'Sawitzki',
+    'firstname': ['Rainer', 'Ulrich'],
     'height': 183,
     'address': {
-     'city': 'München',
-     'street': 'Marienplatz'
+        'city': 'München',
+        'street': 'Marienplatz'
     }
- }
-
- let meier_document = {
-    'lastname': 'Meier', 
-    'firstname': 'Hannah', 
+},
+{
+    'lastname': 'Meier',
+    'firstname': 'Hannah',
     'height': 183,
     'address': {
-     'city': 'München',
-     'street': 'Marienplatz'
+        'city': 'München',
+        'street': 'Marienplatz'
     }
- }
+}
+])
 
-
- db.data.insertMany([sawitzki_document, meier_document])
-
- db.data.find()
- 
+db.data.find()
